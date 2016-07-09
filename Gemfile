2f2213e6e484694aa5aed89e45a33792d4accac2
tpb1908 <theo1908phone@gmail.com>
2016-07-09 15:11:38 +0100

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -17,6 +17,9 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
+gem 'bootstrap-sass', '3.2.0.2'
+
+gem 'rails-controller-testing'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about', as: 'about'
   get  '/contact', to: 'static_pages#contact', as: 'contact'
   get  '/signup',  to: 'users#new', as: 'signup'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/signup',  to: 'users#create'
+  resources :users
 end
