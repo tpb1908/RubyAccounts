5669a9af45a1da2ba79eace945defcfe1abcd0db
tpb1908 <theo1908phone@gmail.com>
2016-07-09 13:08:53 +0100

    Empty signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get  '/help',    to: 'static_pages#help', as: 'help_path'
-  get  '/about',   to: 'static_pages#about', as: 'about_path'
-  get  '/contact', to: 'static_pages#contact', as: 'contact_path'
-  get  '/signup',  to: 'users#new', as: 'login_path'
+  get  '/help',    to: 'static_pages#help', as: 'help'
+  get  '/about',   to: 'static_pages#about', as: 'about'
+  get  '/contact', to: 'static_pages#contact', as: 'contact'
+  get  '/signup',  to: 'users#new', as: 'signup'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
