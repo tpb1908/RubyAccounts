287fe9adf168b84e2b26e9aa522efbf3374e9625
tpb1908 <theo1908phone@gmail.com>
2016-07-09 19:36:18 +0100

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    attr_accessor :remember_token
+
 	before_save {self.email = email.downcase} #Inside the User model, the self is option on the right side
 	validates(:name, presence: true, length: { maximum: 50 })
 	VALIDATE_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -12,5 +14,26 @@ class User < ApplicationRecord
 		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
 		BCrypt::Password.create(string, cost: cost)
 	end
+
+    #New random token for cookie
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    #Remembers a user in the database for use in persistent sessions
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    def forget
+        update_attribute(:remember_digest, nil)
+    end
+
+    #Returns true if the given token matches the digest
+    def authenticated?(remember_token)
+        return false if remember_digest.nil?
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
 
 end
