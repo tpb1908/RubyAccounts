84f10112cd319ef79a11683b41e9c1d066f4430e
tpb1908 <theo1908phone@gmail.com>
2016-07-22 21:01:27 +0100

    Added route for new word set

--- app/controllers/word_controller.rb
+++ app/controllers/word_controller.rb
@@ -1,5 +1,5 @@
 class WordController < ApplicationController
-    before_action: :logged_in_user , only:[:new, :create, :edit, :destroy]
+    before_action :logged_in_user , only: [:new, :create, :edit, :destroy]
 
     def create
         @wordset = WordSet.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   get "word_controller/get", to: 'word#get'
 
+  get '/tests/new', to: 'word#new'
+
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
