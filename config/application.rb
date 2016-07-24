47645307e0974818353a231324bf5b410cee5598
tpb1908 <theo1908phone@gmail.com>
2016-07-24 16:35:50 +0100

    Removed initializer in order to reset heroku db

--- config/application.rb
+++ config/application.rb
@@ -12,13 +12,7 @@ module App
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.after_initialize  do
-      $users_online = 0
-      User.all.each do |n|
-        if n.logged_in then $users_online += 1 end
-      end
-      puts 'Users online ' + $users_online.to_s
-    end
+
   end
 
 
