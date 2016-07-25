479c2e56347603dbefb9c46eac63b4e776892d6a
tpb1908 <theo1908phone@gmail.com>
2016-07-25 12:45:55 +0100

    Updated mailer test exepected messages

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -6,7 +6,7 @@ class UserMailerTest < ActionMailer::TestCase
     user = users(:test1)
     user.activation_token = User.new_token
     mail = UserMailer.account_activation(user)
-    assert_equal "Account activation", mail.subject
+    assert_equal "TypingAnalysis account activation", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["noreply@typinganalysis.com"], mail.from
     assert_match user.name, mail.body.encoded
@@ -18,7 +18,7 @@ class UserMailerTest < ActionMailer::TestCase
     user = users(:test1)
     user.reset_token = User.new_token
     mail = UserMailer.password_reset(user)
-    assert_equal "Password reset", mail.subject
+    assert_equal "TypingAnalysis password reset", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["noreply@typinganalysis.com"], mail.from
     assert_match user.reset_token, mail.body.encoded
