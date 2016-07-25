ff87877ea39af295e39a17d09ccabe5899b76180
tpb1908 <theo1908phone@gmail.com>
2016-07-25 15:11:52 +0100

    Removed startup task to reset heroku

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,9 @@ module App
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.after_initialize do
-        CountUsers.count
-    end
+    # config.after_initialize do
+    #     CountUsers.count
+    # end
 
   end
 
