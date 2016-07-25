ff5a1edb40cd6850307c3cb3f85c1a42636c42cb
tpb1908 <theo1908phone@gmail.com>
2016-07-25 21:39:09 +0100

    Even more migration updates

--- db/migrate/20160725201220_add_average_wpm_to_users.rb
+++ db/migrate/20160725201220_add_average_wpm_to_users.rb
@@ -1,5 +1,5 @@
 class AddAverageWpmToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :average_wpm, :integer
+    add_column :users, :average_wpm, :integer, :default => 0
   end
 end
