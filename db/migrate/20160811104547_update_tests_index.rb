c27c330a34e5ab932e95e7f6b3c0f90c6cd29795
tpb1908 <theo1908phone@gmail.com>
2016-08-11 13:43:13 +0100

    Removed unnecessary migration for Heroku

--- db/migrate/20160811104547_update_tests_index.rb
+++
@@ -1,7 +0,0 @@
-class UpdateTestsIndex < ActiveRecord::Migration[5.0]
-  def change
-  	add_column :tests, :user_id, :integer
-  	remove_index :tests, :created_at
-  	add_index :tests, [:user_id, :created_at]
-  end
-end
