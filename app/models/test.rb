12ad39a7214382bb31ee0437215d2a100e6b08ad
tpb1908 <theo1908phone@gmail.com>
2016-08-11 13:40:54 +0100

    Set up model relationships

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,18 @@
 class Test < ApplicationRecord
 
+	validates(:cpm, :inclusion => 0..1000)
+
+	serialize :params, JSON
+
 	belongs_to :user
+	belongs_to :word_set
 	has_one :word_set
 
+
+	private 
+
+		def valid_params
+			puts 'Checking params'
+			puts self.params
+		end
 end
