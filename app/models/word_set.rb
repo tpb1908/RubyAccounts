041edd35e48c846bed76dc24164439206ba7b7cf
tpb1908 <theo1908phone@gmail.com>
2016-08-11 16:21:32 +0100

    Fixed typo in model

--- app/models/word_set.rb
+++ app/models/word_set.rb
@@ -1,6 +1,6 @@
 class WordSet < ApplicationRecord
     belongs_to :user
-    has_many: :test
+    has_many :test
     default_scope -> { order(created_at: :desc) }
     validates :user_id, presence: true
     validates :name, presence: true, length: { maximum: 30}
